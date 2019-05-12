025ede5c38919d45e8b47bf891a0c20d09dbfd3a
SamUndefined <sam.undefined.dev@gmail.com>
2019-05-12 12:53:28 -0500

    Properly configure sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,8 @@ const postCssPlugins = require('./postcss-config.js');
 
 module.exports = {
   siteMetadata: {
-    siteUrl: siteConfig.siteUrl,
     url: siteConfig.url,
+    siteUrl: siteConfig.url,
     title: siteConfig.title,
     subtitle: siteConfig.subtitle,
     copyright: siteConfig.copyright,
@@ -139,7 +139,7 @@ module.exports = {
           {
             site {
               siteMetadata {
-                url
+                siteUrl
               }
             }
             allSitePage(
@@ -157,7 +157,7 @@ module.exports = {
         `,
         output: '/sitemap.xml',
         serialize: ({ site, allSitePage }) => allSitePage.edges.map((edge) => ({
-          url: site.siteMetadata.url + edge.node.path,
+          url: site.siteMetadata.siteUrl + edge.node.path,
           changefreq: 'daily',
           priority: 0.7
         }))
